ddd84b5e03c9810478d69f9219be4b99dc133e2f
Daniel Justin <danieljmuleke@gmail.com>
2026-05-05 22:11:56 +0300

    feat: bootstrap module entry with constants, reducer, main menu, and translations

---
+++ src/menus/AssetMainMenu.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-props-no-spreading */
+
+import React from 'react';
+import { injectIntl } from 'react-intl';
+import { connect } from 'react-redux';
+import { Devices } from '@material-ui/icons';
+import { formatMessage, MainMenuContribution, withModulesManager } from '@openimis/fe-core';
+import {
+  RIGHT_ASSET_SEARCH,
+  ASSET_MANAGEMENT_MAIN_MENU_CONTRIBUTION_KEY,
+} from '../constants';
+
+function AssetMainMenu(props) {
+  const entries = [
+    {
+      text: formatMessage(props.intl, 'assetManagement', 'menu.assets'),
+      icon: <Devices />,
+      route: '/assets',
+      filter: (rights) => rights.includes(RIGHT_ASSET_SEARCH),
+      id: 'assetManagement.assets',
+    },
+  ];
+  entries.push(
+    ...props.modulesManager
+      .getContribs(ASSET_MANAGEMENT_MAIN_MENU_CONTRIBUTION_KEY)
+      .filter((c) => !c.filter || c.filter(props.rights)),
+  );
+
+  return (
+    <MainMenuContribution
+      {...props}
+      header={formatMessage(props.intl, 'assetManagement', 'mainMenu')}
+      entries={entries}
+      menuId="AssetMainMenu"
+    />
+  );
+}
+
+const mapStateToProps = (state) => ({
+  rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : [],
+});
+
+export default injectIntl(withModulesManager(connect(mapStateToProps)(AssetMainMenu)));
---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+// Disabled due to consistency with other modules
+/* eslint-disable default-param-last */
+
+const initialState = {};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
