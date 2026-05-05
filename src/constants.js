c9834d9521e16e38c7c3d8c3981b8a067e0422fe
Daniel Justin <danieljmuleke@gmail.com>
2026-05-05 22:11:56 +0300

    feat: bootstrap module entry with constants, reducer, main menu, and translations

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const MODULE_NAME = 'assetManagement';
+
+export const ASSET_MANAGEMENT_MAIN_MENU_CONTRIBUTION_KEY = 'assetManagement.MainMenu.contribution';
+
+// Rights — must match backend AssetsConfig (placeholders to be confirmed once backend is wired)
+export const RIGHT_ASSET_SEARCH = 101501;
+export const RIGHT_ASSET_VIEW = 101502;
+export const RIGHT_ASSET_CREATE = 101503;
+export const RIGHT_ASSET_UPDATE = 101504;
+export const RIGHT_ASSET_DELETE = 101505;
+export const RIGHT_ASSET_ASSIGN = 101506;
+export const RIGHT_ASSET_UNASSIGN = 101507;
+export const RIGHT_ASSET_HISTORY = 101508;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Disable due to core architecture
+/* eslint-disable camelcase */
+/* eslint-disable import/prefer-default-export */
+import flatten from 'flat';
+import messages_en from './translations/en.json';
+import reducer from './reducer';
+import AssetMainMenu from './menus/AssetMainMenu';
+import { MODULE_NAME } from './constants';
+
+const ROUTE_ASSETS = 'assets';
+const ROUTE_ASSET = 'assets/asset';
+const ROUTE_ASSET_HISTORY = 'assets/asset/history';
+
+const DEFAULT_CONFIG = {
+  translations: [{ key: 'en', messages: flatten(messages_en) }],
+  reducers: [{ key: MODULE_NAME, reducer }],
+  'core.MainMenu': [{ name: 'AssetMainMenu', component: AssetMainMenu }],
+  'core.Router': [
+    // TODO: page routes will be added here
+  ],
+  refs: [
+    { key: 'assetManagement.route.assets', ref: ROUTE_ASSETS },
+    { key: 'assetManagement.route.asset', ref: ROUTE_ASSET },
+    { key: 'assetManagement.route.assetHistory', ref: ROUTE_ASSET_HISTORY },
+  ],
+};
+
+export const AssetManagementModule = (cfg) => ({ ...DEFAULT_CONFIG, ...cfg });
