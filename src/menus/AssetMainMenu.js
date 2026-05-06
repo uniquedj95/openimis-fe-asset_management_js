8f6d7f3e6942277fe18941acc6714db7f30f11eb
Daniel Justin <danieljmuleke@gmail.com>
2026-05-06 10:41:38 +0300

    chore: remove unnecessary blank line in AssetMainMenu.js

--- src/menus/AssetMainMenu.js
+++ src/menus/AssetMainMenu.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-props-no-spreading */
-
 import React from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
