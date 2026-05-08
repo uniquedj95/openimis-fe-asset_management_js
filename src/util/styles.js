f3ef4e687f21ee3f27bad32e5506b7d05b03198c
Daniel Justin <danieljmuleke@gmail.com>
2026-05-08 13:53:19 +0300

    feat: implement AssetFilter component with various filtering options

---
+++ src/util/styles.js
@@ -0,0 +1,12 @@
+export const defaultPageStyles = (theme) => ({
+  page: theme.page,
+});
+
+export const defaultFilterStyles = (theme) => ({
+  form: {
+    padding: 0,
+  },
+  item: {
+    padding: theme.spacing(1),
+  },
+});
